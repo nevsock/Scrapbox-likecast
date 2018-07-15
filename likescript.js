09358855d56bf2e9a990b0f269b33094a60338f3
nevsock <reiwon.yamabuki@gmail.com>
2018-07-15 17:41:46 +0900

    各ページごとのいいねを扱えるように修正

--- likescript.js
+++ likescript.js
@@ -1,30 +1,37 @@
+let url = location.href
+
+//新しくURLに紐づけされたカウントを設定する
+const setCount = function(url) {
+  let initialize = 0
+  localStorage.setItem(url, initialize)
+}
+
+//カウントをロード、なければ生成
 const loadCount = function() {
-  if(!localStorage.getItem('count')){
-    setCount()
+  if(!localStorage.getItem(url)){
+    setCount(url)
   }
-  return localStorage.getItem('count')
+  button.innerHTML = '♡ ' + localStorage.getItem(url)
 }
 
-const setCount = function() {
-  let _count = 0
-  localStorage.setItem('count', _count)
-}
-
+//ボタン作成・bodyに追加
 let button = document.body.appendChild(document.createElement('button'))
 let clearButton = document.body.appendChild(document.createElement('button'))
+//idでcssを適応できるように
 button.id = 'like_button'
 clearButton.id = 'clear_button'
-let count = loadCount()
-
-button.innerHTML = '♡ ' + count
+//ボタン内容生成
 clearButton.innerHTML = 'clear'
 
+//いいねボタンを押した時の挙動
 const plusCount = function() {
-  let _count = localStorage.getItem('count')
-  count = Number(_count) + 1
-  localStorage.setItem('count', count)
-  button.innerHTML = '♡ ' + count
+  localStorage.setItem(url, Number(localStorage.getItem(url)) + 1)
+  loadCount()
 }
 
+//クリックでイベント発火
 button.addEventListener('click', ()=>{plusCount()}, false)
-clearButton.addEventListener('click', ()=>{localStorage.clear(), setCount()}, false)
+clearButton.addEventListener('click', ()=>{localStorage.setItem(url, 0); loadCount();}, false)
+
+//ロード時に呼び出し
+window.onload = loadCount()
