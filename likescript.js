4d9eeace2d0b9ad1225c88528b55c45a79a6a1f3
nevsock <reiwon.yamabuki@gmail.com>
2018-07-16 17:46:27 +0900

    テキスト入力できるように

--- likescript.js
+++ likescript.js
@@ -1,38 +1,52 @@
 //URL読み込み
 let url = location.href
+let frame = document.body.appendChild(document.createElement('div'))
+frame.id = 'frame'
 
-//新しくURLに紐づけされたカウントを設定する
-const setCount = function(url) {
-  let initialize = 0
-  localStorage.setItem(url, initialize)
-}
-
-//カウントをロード、なければ生成
-const loadCount = function() {
-  if(!localStorage.getItem(url)){
-    setCount(url)
+//リアクションをロード
+const loadReaction = function() {
+  if(localStorage.getItem(url)){
+    let data = JSON.parse(localStorage.getItem(url))
+    for(let i=0; i<data.length; i++){
+      let reaction = document.appendChild(document.createElement('p'))
+      reaction.className = 'reactions'
+      reaction.innerHTML = data[i].content
+    }
   }
-  button.innerHTML = '♡ ' + localStorage.getItem(url)
 }
 
-//ボタン作成・bodyに追加
-let button = document.body.appendChild(document.createElement('button'))
-let clearButton = document.body.appendChild(document.createElement('button'))
+//ボタン作成・frameに追加
+let clearButton = frame.appendChild(document.createElement('button'))
+let reactionViewer = frame.appendChild(document.createElement('div'))
+let plusButton = frame.appendChild(document.createElement('button'))
+let textBox = frame.appendChild(document.createElement('input'))
+textBox.setAttribute('type', 'text')
 //idでcssを適応できるように
-button.id = 'like_button'
 clearButton.id = 'clear_button'
+reactionViewer.id = 'reaction_viewer'
+plusButton.id = 'plus_button'
+textBox.id = 'textbox'
 //ボタン内容生成
 clearButton.innerHTML = 'clear'
+plusButton.innerHTML = '+'
 
 //いいねボタンを押した時の挙動
-const plusCount = function() {
-  localStorage.setItem(url, Number(localStorage.getItem(url)) + 1)
-  loadCount()
+const plusReaction = function() {
+  let content = document.getElementById('textBox').value
+  let addData = { reaction : string(content), count : 0 }
+  if(!localStorage.getItem(url)){
+    let data = new Array()
+    localStorage.setItem(url, JSON.stringify(data.push(addData)))
+  } else {  
+    let data = JSON.parse(localStorage.getItem(url)).push(addData)
+    localStorage.setItem(url, JSON.stringify(data))
+  }
+  loadReaction()
 }
 
 //クリックでイベント発火
-button.addEventListener('click', ()=>{plusCount()}, false)
-clearButton.addEventListener('click', ()=>{localStorage.setItem(url, 0); loadCount();}, false)
+plusButton.addEventListener('click', ()=>{plusReaction()}, false)
+clearButton.addEventListener('click', ()=>{localStorage.setItem(url, '[]'); loadReaction();}, false)
 
 //ロード時に呼び出し
-window.onload = loadCount()
+window.onload = loadReaction()
